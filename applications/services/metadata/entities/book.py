f0555ea89453e86df97bb53b109d70dff21e20fe
Svensmark <emil.svensmark@gmail.com>
2021-12-09 14:21:35 +0100

    Branch out

--- applications/services/metadata/entities/book.py
+++ applications/services/metadata/entities/book.py
@@ -1,4 +1,32 @@
 
+from dataclasses import dataclass
+
+@dataclass
+class Book:
+    """Class for book-metadata"""
+    id: int
+    title: str
+    author: str
+    rating: int
+
+    def __init__(self, id: int, title: str, author: str, rating: int):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.rating = rating
+
+    def create():
+        return 0
+
+    def read():
+        return 0
+
+    def update():
+        return 0
+    
+    def delete():
+        return 0
+
 def getBookInfo(x):
     y = x+x
     return y
