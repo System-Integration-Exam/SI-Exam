5b6213ad91877c7488da0bce57fb392224ce2c59
Sofus Hilfling Nielsen <sofushn@hotmail.dk>
2021-12-12 16:09:47 +0100

    Added db migration when starting reservation service

--- applications/services/reservation/Program.cs
+++ applications/services/reservation/Program.cs
@@ -19,4 +19,10 @@ var app = builder.Build();
 app.MapGrpcService<ReservationService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ReservationContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
