b35750bce6f9975a46c755442d91f71a766fe2d5
apatheticfrog <apathetic_frog@protonmail.com>
2021-12-12 16:03:29 +0100

    gateway fixes. need poetry build for testing

--- applications/services/gateway/run_flask.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export FLASK_APP=src/main.py
-flask run --host=0.0.0.0 --port=10000
-
-
--- applications/services/gateway/src/clients/warehouse.py
+++ applications/services/gateway/src/clients/warehouse.py
@@ -16,15 +16,14 @@ def _create_stub():
     return store_pb2_grpc.StoreStub(channel)
 
 
-def create_store(new_store):
+def create_store(new_store_json):
     return _create_stub().CreateStore(
         store_pb2.CreateStoreRequest(
-            address=new_store.address,
-            phone_number=new_store.phone_number,
-            email=new_store.email
+            address=new_store_json["address"],
+            phone_number=new_store_json["phone_number"],
+            email=new_store_json["email"],
         )
-    )
-
+    ).msg
 
 def read_store(store_id):
     response =  _create_stub().ReadStore(
@@ -32,30 +31,48 @@ def read_store(store_id):
             id=store_id
         )
     )
-    return JSON.dumps(Store.from_grpc_response(response).__dict__)
+    return JSON.dumps(response.__dict__)
 
-def update_store(store_id, store):
+def update_store(store_id, store_json):
     return _create_stub().UpdateStore(
         store_pb2.UpdateStoreRequest(
             id=store_id,
-            store=store_pb2.StoreObject(
-                address=store.address,
-                phone_number=store.phone_number,
-                email=store.email
-            )
+            address=store_json["address"],
+            phone_number=store_json["phone_number"],
+            email=store_json["email"]
         )
-    )
+    ).msg
+
+def update_store_by_address(address_match, store):
+    return _create_stub().UpdateStore(
+        store_pb2.UpdateStoreRequest(
+            address_match=address_match,
+            address_update=store["address"],
+            phone_number=store["phone_number"],
+            email=store["email"]
+        )
+    ).msg
 
 def delete_store(store_id):
     return _create_stub().DeleteStore(
-        store_pb2.DeleteStore(
+        store_pb2.DeleteStoreRequest(
             id=store_id,
         )
-    )
+    ).msg
+
+
+def delete_store_by_address(address):
+    return _create_stub().DeleteStoreByAddress(
+        store_pb2.DeleteStoreByAddressRequest(
+            address=address
+        )
+    ).msg
 
 def read_store_list():
     response = _create_stub().ReadStoreList(store_pb2.ReadStoreListRequest())
     return JSON.dumps(Store.from_grpc_list_response(response))
+
+################
 
 def add_book_to_store(store_id, book_id):
     return _create_stub().AddBookToStore(
@@ -63,7 +80,7 @@ def add_book_to_store(store_id, book_id):
             store_id=store_id,
             book_id=book_id,
         )
-    )
+    ).msg
 
 def remove_book_from_store(store_id, book_id):
     return _create_stub().RemoveBookFromStore(
@@ -71,7 +88,7 @@ def remove_book_from_store(store_id, book_id):
             store_id=store_id,
             book_id=book_id,
         )
-    )
+    ).msg
 
 def get_amount_of_specific_book_from_store(store_id, book_id):
     return _create_stub().GetAmountOfSpecificBookFromStore(
@@ -79,7 +96,9 @@ def get_amount_of_specific_book_from_store(store_id, book_id):
             store_id=store_id,
             book_id=book_id,
         )
-    )
+    ).amount
+
+##########
 
 def add_vinyl_to_store(store_id, vinyl_id):
     return _create_stub().AddVinylToStore(
@@ -87,7 +106,7 @@ def add_vinyl_to_store(store_id, vinyl_id):
             store_id=store_id,
             vinyl_id=vinyl_id
         )
-    )
+    ).msg
 
 def remove_vinyl_from_store(store_id, vinyl_id):
     return _create_stub().RemoveVinylFromStore(
@@ -95,7 +114,7 @@ def remove_vinyl_from_store(store_id, vinyl_id):
             store_id=store_id,
             vinyl_id=vinyl_id
         )
-    )
+    ).msg
 
 def get_amount_of_specific_vinyl_from_store(store_id, vinyl_id):
     return _create_stub().GetAmountOfSpecificVinylFromStore(
@@ -103,7 +122,7 @@ def get_amount_of_specific_vinyl_from_store(store_id, vinyl_id):
             store_id=store_id,
             vinyl_id=vinyl_id
         )
-    )
+    ).amount
 
 
 
