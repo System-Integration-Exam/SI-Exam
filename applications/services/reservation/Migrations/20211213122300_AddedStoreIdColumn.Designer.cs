cc150a35a16cac409d67d39878aa9fbd70d8cc51
Sofus Hilfling Nielsen <sofushn@hotmail.dk>
2021-12-13 13:28:12 +0100

    Created new migration version

--- applications/services/reservation/Migrations/20211213122300_AddedStoreIdColumn.Designer.cs
+++ applications/services/reservation/Migrations/20211213122300_AddedStoreIdColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reservation.Persistency;
 
@@ -10,9 +11,10 @@ using Reservation.Persistency;
 namespace Reservation.Migrations
 {
     [DbContext(typeof(ReservationContext))]
-    partial class ReservationContextModelSnapshot : ModelSnapshot
+    [Migration("20211213122300_AddedStoreIdColumn")]
+    partial class AddedStoreIdColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.0");
