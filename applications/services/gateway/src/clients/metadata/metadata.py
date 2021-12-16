5b23140fac51ac59e26393954d8a935d2b16c2d8
Svensmark <emil.svensmark@gmail.com>
2021-12-16 02:29:52 +0100

    Added methods for all entity classes in gateway client for metadata

--- applications/services/gateway/src/clients/metadata/metadata.py
+++ applications/services/gateway/src/clients/metadata/metadata.py
@@ -26,14 +26,14 @@ def _createSongStub():
     )
     return song_pb2_grpc.SongStub(channel)
     
-def _createvinylStub():
+def _createVinylStub():
     channel = grpc.insecure_channel(
         f"{_CLIENT_CONFIG['host']}:{_CLIENT_CONFIG['port']}"
     )
     return vinyl_pb2_grpc.VinylStub(channel)
 
 
-# book client
+# book methods
 def createBook(json):
     return (
         _createBookStub()
@@ -50,8 +50,8 @@ def createBook(json):
 def getBookById(json):
     return (
         _createBookStub()
-        .createBook(
-            book_pb2.CreateBookRequest(
+        .getBookById(
+            book_pb2.GetBookByIdRequest(
                 id=json["id"]
             )
         )
@@ -61,8 +61,8 @@ def getBookById(json):
 def updateBook(json):
     return (
         _createBookStub()
-        .createBook(
-            book_pb2.CreateBookRequest(
+        .updateBook(
+            book_pb2.UpdateBookRequest(
                 id=json["id"],
                 title=json["title"],
                 author=json["author"],
@@ -75,8 +75,8 @@ def updateBook(json):
 def deleteBookById(json):
     return (
         _createBookStub()
-        .createBook(
-            book_pb2.CreateBookRequest(
+        .deleteBookById(
+            book_pb2.DeleteBookByIdRequest(
                 id=json["id"]
             )
         )
@@ -86,11 +86,131 @@ def deleteBookById(json):
 def getAllBooks():
     return (
         _createBookStub()
-        .createBook(
-            book_pb2.CreateBookRequest(
+        .getAllBooks(
+            book_pb2.GetAllBooksRequest()
+        )
+        .msg
+    )
+
+
+# song methods
+def createSong(json):
+    return (
+        _createSongStub()
+        .createSong(
+            song_pb2.CreateSongRequest(
+                title=json["title"],
+                duration_sec=json["duration_sec"],
+                vinyl_id=json["vinyl_id"]
             )
         )
         .msg
     )
+
+def getSongById(json):
+    return (
+        _createSongStub()
+        .getSongById(
+            song_pb2.GetSongByIdRequest(
+                id=json["id"]
+            )
+        )
+        .msg
+    )
+
+def updateSong(json):
+    return (
+        _createSongStub()
+        .updateSong(
+            song_pb2.UpdateSongRequest(
+                id=json["id"],
+                title=json["title"],
+                duration_sec=json["duration_sec"],
+                vinyl_id=json["vinyl_id"]
+            )
+        )
+        .msg
+    )
+
+def deleteSongById(json):
+    return (
+        _createSongStub()
+        .deleteSongById(
+            song_pb2.DeleteSongByIdRequest(
+                id=json["id"]
+            )
+        )
+        .msg
+    )
+
+def getAllSongs():
+    return (
+        _createSongStub()
+        .getAllSongs(
+            song_pb2.GetAllSongsRequest()
+        )
+        .msg
+    )
+
+
+# song methods
+def createVinyl(json):
+    return (
+        _createVinylStub()
+        .createVinyl(
+            vinyl_pb2.CreateVinylRequest(
+                artist=json["artist"],
+                genre=json["genre"]
+            )
+        )
+        .msg
+    )
+
+def getVinylById(json):
+    return (
+        _createVinylStub()
+        .getVinylById(
+            vinyl_pb2.GetVinylByIdRequest(
+                id=json["id"]
+            )
+        )
+        .msg
+    )
+
+def updateVinyl(json):
+    return (
+        _createVinylStub()
+        .updateVinyl(
+            vinyl_pb2.UpdateVinylRequest(
+                id=json["id"],
+                artist=json["artist"],
+                genre=json["genre"]
+            )
+        )
+        .msg
+    )
+
+def deleteVinylById(json):
+    return (
+        _createVinylStub()
+        .deleteVinylById(
+            vinyl_pb2.DeleteVinylByIdRequest(
+                id=json["id"]
+            )
+        )
+        .msg
+    )
+
+def getAllVinyl():
+    return (
+        _createVinylStub()
+        .getAllVinyl(
+            vinyl_pb2.GetAllVinylRequest()
+        )
+        .msg
+    )
+
+
+
 
 
