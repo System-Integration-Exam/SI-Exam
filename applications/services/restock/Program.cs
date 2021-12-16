11a48c4e738203651b1f2d55856a81428bf1068e
Sofus Hilfling Nielsen <sofushn@hotmail.dk>
2021-12-16 09:07:41 +0100

    Added task handler for camunda service task to restock service

--- applications/services/restock/Program.cs
+++ applications/services/restock/Program.cs
@@ -1,9 +1,12 @@
+using Camunda.Worker;
+using Camunda.Worker.Client;
 using Restock.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+builder.Services.AddExternalTaskClient(client => client.BaseAddress = new Uri("http://localhost:10000/engine-rest"));
+builder.Services.AddCamundaWorker("dotnetWorker")
+    .AddHandler<CamundaTaskHandler>();
 
 // Add services to the container.
 builder.Services.AddGrpc();
