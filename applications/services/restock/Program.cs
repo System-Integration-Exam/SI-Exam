453b51cfb67d3314e85edba33787a6edce222333
Sofus Hilfling Nielsen <sofushn@hotmail.dk>
2021-12-16 11:31:31 +0100

    Implemented RequestRestock from restock.proto

--- applications/services/restock/Program.cs
+++ applications/services/restock/Program.cs
@@ -4,9 +4,13 @@ using Restock.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddExternalTaskClient(client => client.BaseAddress = new Uri("http://localhost:10000/engine-rest"));
+string camundaBaseUrl = builder.Configuration.GetValue<string>("CamundaRestUrl");
+Action<HttpClient> clientBuilder = client => client.BaseAddress = new Uri(camundaBaseUrl);
+
+builder.Services.AddExternalTaskClient(clientBuilder);
 builder.Services.AddCamundaWorker("dotnetWorker")
     .AddHandler<CamundaTaskHandler>();
+builder.Services.AddHttpClient<RestockService>(clientBuilder);
 
 // Add services to the container.
 builder.Services.AddGrpc();
--- applications/services/restock/Services/RestockService.cs
+++ applications/services/restock/Services/RestockService.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Camunda.Worker;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Restock.Protos;
@@ -7,13 +10,49 @@ namespace Restock.Services;
 public class RestockService : RestockGrpc.RestockGrpcBase
 {
     private readonly ILogger<RestockService> _logger;
-    public RestockService(ILogger<RestockService> logger)
+    private readonly HttpClient _httpClient;
+    private readonly JsonSerializerOptions _jsonOptions;
+
+    public RestockService(ILogger<RestockService> logger, HttpClient httpClient)
     {
         _logger = logger;
+        _httpClient = httpClient;
+        _jsonOptions = new()
+        {
+            Converters = { new JsonStringEnumConverter() }, 
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
     }
 
-    public override Task<Empty> RequestRestock(RestockRequest request, ServerCallContext context)
+    public override async Task<Empty> RequestRestock(RestockRequest request, ServerCallContext context)
     {
-        return base.RequestRestock(request, context);
+        var requestBody = new { 
+            variables = new RequestVariables(
+                request.ItemType.ToString().ToLower(),
+                request.RequestText,
+                request.ExistingItemCount,
+                request.StoreId
+            )};
+
+        await _httpClient.PostAsJsonAsync("process-definition/key/Restock_Process/submit-form", requestBody, _jsonOptions);
+        _logger.LogInformation("{jsonBody}", JsonSerializer.Serialize(requestBody, _jsonOptions));
+        return new Empty();
+    }
+
+    public class RequestVariables
+    {
+        public RequestVariables(string itemType, string requestText, long existingItemCount, long storeId)
+        {
+            ItemType = Variable.String(itemType);
+            RequestText = Variable.String(requestText);
+            ExistingItemCount = Variable.Long(existingItemCount);
+            StoreId = Variable.Long(storeId);
+        }
+
+        public Variable ItemType { get; }
+        public Variable RequestText { get; }
+        public Variable ExistingItemCount { get; }
+        public Variable StoreId { get; }
+
     }
 }
